7b7d65f15f3b8f5b7db979c532ea3582e412a959
glyphack <sh.hooshyari@gmail.com>
2022-01-17 01:45:21 +0330

    Add jwt secret to env config

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,6 +10,8 @@ func InitConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("koal")
+	viper.BindEnv("jwt_secret")
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %w \n", err))
