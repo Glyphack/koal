61cff713608a5deead5a9c79bf2e387d990254c5
glyphack <sh.hooshyari@gmail.com>
2022-04-27 22:48:48 +0430

    Fix test todo update item test ObjectId

--- internal/module/todo/infrastructure/db_todo_test.go
+++ internal/module/todo/infrastructure/db_todo_test.go
@@ -147,7 +147,7 @@ func (suite *Suite) Test_db_todo_UpdateItem() {
 			},
 			args: args{
 				ctx:    context.Background(),
-				itemId: "f47ac10b-58cc-0372-8567-0e02b2c3d479",
+				itemId: "f47ac10b-58cc-0372-8567-0e02b2c3d480",
 				updatedItem: &todoitem.TodoItem{
 					UUId:    uuid.MustParse("f47ac10b-58cc-0372-8567-0e02b2c3d480"),
 					Title:   "title",
