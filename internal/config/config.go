73898b0930c3ac18eea74666b7f13c678e89c475
glyphack <sh.hooshyari@gmail.com>
2022-05-01 12:30:59 +0430

    Refactor config structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,17 +6,29 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	allowedOrigin string `mapstructure:"ALLOWED_ORIGIN"`
+	postgresUri   string `mapstructure:"POSTGRES_URI"`
+	jwtSecret     string `mapstructure:"JWT_SECRET"`
+	debug         bool   `mapstructure:"DEBUG"`
+	sentryDSN     string `mapstructure:"SENTRY_DSN"`
+}
+
 func InitConfig() {
 	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
+	viper.SetConfigType("env")
 	viper.AddConfigPath(".")
-	viper.SetEnvPrefix("koal")
-	viper.BindEnv("jwt_secret")
-	viper.BindEnv("postgres_uri")
-	viper.BindEnv("debug")
-	viper.SetDefault("debug", true)
+
+	viper.AutomaticEnv()
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+	
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		panic(fmt.Errorf("Fatal parsing config file: %w \n", err))
 	}
 }
