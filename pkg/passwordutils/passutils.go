2a661376ed22d24ce498484348a0343fd4ca1275
glyphack <sh.hooshyari@gmail.com>
2022-05-03 07:49:44 +0430

    Rename function to make it clear what it returns

--- pkg/passwordutils/passutils.go
+++ pkg/passwordutils/passutils.go
@@ -7,7 +7,7 @@ func HashPassword(password string) (string, error) {
 	return string(bytes), err
 }
 
-func CheckPasswordHash(password, hash string) bool {
+func IsPasswordAndHashEqual(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
