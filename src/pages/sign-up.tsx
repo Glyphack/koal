c8e182347aac886a138b0cc68ee0d1375f248c32
frustak <soulburn007@gmail.com>
2022-06-04 13:58:53 +0430

    Refactor sign up page

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,30 +1,33 @@
-import { AxiosError } from 'axios'
 import { useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
-import { api, ResponseError } from '../api'
-import { Field } from '../features/ui'
-
-interface SignUpFormValues {
-	email: string
-	password: string
-}
+import { Link } from 'react-router-dom'
+import { api } from '../api'
+import { useAuthorize } from '../features/auth'
+import { ErrorMessage, Field, Loader } from '../features/ui'
 
 export function SignUpPage() {
-	const form = useForm<SignUpFormValues>({ defaultValues: { email: '', password: '' } })
-	const signUpMutation = useMutation(api.signUp)
-	const signUp = form.handleSubmit((values: SignUpFormValues) => signUpMutation.mutate(values))
+	const authorize = useAuthorize()
+	const form = useForm({ defaultValues: { email: '', password: '' } })
+	const signUpMutation = useMutation(api.signUp, {
+		onSuccess: (data) => {
+			const token = data.data.token
+			authorize(token)
+		},
+	})
+	const signUp = form.handleSubmit((values) => signUpMutation.mutate(values))
 
 	return (
 		<main>
 			<form onSubmit={signUp}>
 				<Field type="text" placeholder="Email" {...form.register('email')} />
 				<Field type="password" placeholder="Password" {...form.register('password')} />
-				<button type="submit">Sign Up</button>
-				{signUpMutation.isLoading && <p>Submitting...</p>}
+				<button type="submit" disabled={signUpMutation.isLoading}>
+					Sign Up
+				</button>
+				<Loader loading={signUpMutation.isLoading} />
 			</form>
-			{signUpMutation.error instanceof AxiosError && (
-				<p>{(signUpMutation.error.response?.data as ResponseError).message}</p>
-			)}
+			<ErrorMessage error={signUpMutation.error} />
+			<Link to="/sign-in">Sign In</Link>
 		</main>
 	)
 }
