b05debcdc668a7754de6e675e808ab4b8dd16e42
frustak <soulburn007@gmail.com>
2022-06-04 23:35:20 +0430

    Implement projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,3 +1,10 @@
+import { ProjectForm, ProjectList } from '../features/project'
+
 export function ProjectsPage() {
-	return <main>projects</main>
+	return (
+		<main>
+			<ProjectForm />
+			<ProjectList />
+		</main>
+	)
 }
