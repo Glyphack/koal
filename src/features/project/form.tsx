e73f417278d4496a44cc9853398dc904a42840f7
frustak <soulburn007@gmail.com>
2022-06-04 23:34:26 +0430

    Implement project form component

---
+++ src/features/project/form.tsx
@@ -0,0 +1,25 @@
+import { useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from 'react-query'
+import { api, QueryKey } from '../../api'
+import { Button, Field } from '../ui'
+
+export function ProjectForm() {
+	const queryClient = useQueryClient()
+	const form = useForm({ defaultValues: { name: '' } })
+	const mutation = useMutation(api.createProject, {
+		onSuccess: () => {
+			queryClient.invalidateQueries(QueryKey.Projects)
+			form.reset()
+		},
+	})
+	const createProject = form.handleSubmit((values) => mutation.mutate(values))
+
+	return (
+		<form onSubmit={createProject}>
+			<Field type="text" placeholder="Project Name" {...form.register('name')} />
+			<Button type="submit" disabled={mutation.isLoading}>
+				Add Project
+			</Button>
+		</form>
+	)
+}
