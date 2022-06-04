e421750941971f361c891916cb9361cae9d02316
frustak <soulburn007@gmail.com>
2022-06-04 23:30:50 +0430

    Add fetch projects and create project endpoints

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,29 +1,31 @@
-import axios from 'axios'
-import Cookies from 'js-cookie'
-import { SignInRequest, SignUpRequest, SignUpResponse } from './types'
+import { privateRequest, publicRequest } from './config'
+import {
+	CreateProjectRequest,
+	FetchProjectsResponse,
+	SignInRequest,
+	SignInResponse,
+	SignUpRequest,
+	SignUpResponse,
+} from './types'
 
-const API_URL = import.meta.env.VITE_API_URL
-const publicRequest = axios.create({ baseURL: API_URL })
-export const privateRequest = axios.create({ baseURL: API_URL })
-privateRequest.interceptors.request.use(
-	(config) => {
-		const token = Cookies.get('token')
-		const bearer = `Bearer ${token}`
-		if (config.headers) config.headers.Authorization = bearer
-		else config.headers = { Authorization: bearer }
-		return config
-	},
-	(error) => {
-		return Promise.reject(error)
-	}
-)
-
-const signUp = async (payload: SignUpRequest) =>
+const signUp = (payload: SignUpRequest) =>
 	publicRequest.post<SignUpResponse>('/auth/register', payload)
 
-const signIn = async (payload: SignInRequest) => publicRequest.post('/auth/login', payload)
+const signIn = (payload: SignInRequest) =>
+	publicRequest.post<SignInResponse>('/auth/login', payload)
+
+const fetchProjects = () => privateRequest.get<FetchProjectsResponse>('/todo/projects')
+
+const createProject = (payload: CreateProjectRequest) =>
+	privateRequest.post('/todo/projects', payload)
 
 export const api = {
 	signUp,
 	signIn,
+	fetchProjects,
+	createProject,
+}
+
+export enum QueryKey {
+	Projects = 'projects',
 }
