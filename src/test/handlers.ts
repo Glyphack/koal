b3b1e0ed0ffe9501fd1a94553c4b6c829def4c63
frustak <soulburn007@gmail.com>
2022-06-04 13:59:00 +0430

    Add more mock handlers

--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -3,7 +3,13 @@ import { rest } from 'msw'
 const API_URL = import.meta.env.VITE_API_URL
 
 export const handlers = [
+	rest.get(`${API_URL}/mock`, (req, res, ctx) => {
+		return res(ctx.status(200))
+	}),
 	rest.post(`${API_URL}/auth/register`, (req, res, ctx) => {
-		return res(ctx.status(200), ctx.json(true))
+		return res(ctx.status(200))
+	}),
+	rest.post(`${API_URL}/auth/login`, (req, res, ctx) => {
+		return res(ctx.status(200))
 	}),
 ]
