06e5c900d1f656594cc22db1af425f5a96a69c62
frustak <soulburn007@gmail.com>
2022-06-04 13:58:30 +0430

    Implement error message component

---
+++ src/features/ui/error-message.tsx
@@ -0,0 +1,16 @@
+import { AxiosError } from 'axios'
+import { ResponseError } from '../../api'
+
+interface ErrorMessageProps {
+	error: unknown
+}
+
+export function ErrorMessage({ error }: ErrorMessageProps) {
+	if (!error) return null
+	let message =
+		error instanceof AxiosError ? (error.response?.data as ResponseError)?.message : null
+	if (!message && error instanceof Error) message = error.message
+	if (!message) return null
+
+	return <p>{message}</p>
+}
