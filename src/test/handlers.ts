22a14e0a39206f1ab8ce3e24e334f8937ad3d44d
frustak <soulburn007@gmail.com>
2022-06-04 23:34:12 +0430

    Add new endpoints mock handlers

--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -6,4 +6,8 @@ export const handlers = [
 	rest.get(`${API_URL}/mock`, (req, res, ctx) => res(ctx.status(200))),
 	rest.post(`${API_URL}/auth/register`, (req, res, ctx) => res(ctx.status(200))),
 	rest.post(`${API_URL}/auth/login`, (req, res, ctx) => res(ctx.status(200))),
+	rest.post(`${API_URL}/todo/projects`, (req, res, ctx) => res(ctx.status(200))),
+	rest.get(`${API_URL}/todo/projects`, (req, res, ctx) =>
+		res(ctx.status(200), ctx.json({ projects: [{ id: '1', name: 'Test Project' }] }))
+	),
 ]
