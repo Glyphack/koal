68f7328e43b5f84eb6b89b10b97c54a40b9837ef
frustak <soulburn007@gmail.com>
2022-06-05 17:11:00 +0430

    Add new api mocks

--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -1,6 +1,10 @@
 import { rest } from 'msw'
+import { FetchProjectResponse, FetchProjectsResponse } from '../api'
 
 const API_URL = import.meta.env.VITE_API_URL
+
+export const mockProject = { id: 'p1', name: 'project 1' }
+export const mockTask = { id: 't1', title: 'task 1', isDone: false, project: mockProject }
 
 export const handlers = [
 	rest.get(`${API_URL}/mock`, (req, res, ctx) => res(ctx.status(200))),
@@ -8,6 +12,15 @@ export const handlers = [
 	rest.post(`${API_URL}/auth/login`, (req, res, ctx) => res(ctx.status(200))),
 	rest.post(`${API_URL}/todo/projects`, (req, res, ctx) => res(ctx.status(200))),
 	rest.get(`${API_URL}/todo/projects`, (req, res, ctx) =>
-		res(ctx.status(200), ctx.json({ projects: [{ id: '1', name: 'Test Project' }] }))
+		res(ctx.status(200), ctx.json<FetchProjectsResponse>({ projects: [mockProject] }))
+	),
+	rest.post(`${API_URL}/todo/items`, (req, res, ctx) => res(ctx.status(200))),
+	rest.delete(`${API_URL}/todo/items/:id`, (req, res, ctx) => res(ctx.status(200))),
+	rest.patch(`${API_URL}/todo/items/:id`, (req, res, ctx) => res(ctx.status(200))),
+	rest.get(`${API_URL}/todo/projects/:id`, (req, res, ctx) =>
+		res(
+			ctx.status(200),
+			ctx.json<FetchProjectResponse>({ info: mockProject, items: [mockTask] })
+		)
 	),
 ]
