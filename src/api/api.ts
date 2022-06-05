b138ad651f8764bb4b29f5a8969f0f7c549c307d
frustak <soulburn007@gmail.com>
2022-06-05 14:01:38 +0430

    Add backend endpoints and types

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,14 @@
 import { privateRequest, publicRequest } from './config'
 import {
 	CreateProjectRequest,
+	CreateTaskRequest,
+	FetchProjectResponse,
 	FetchProjectsResponse,
 	SignInRequest,
 	SignInResponse,
 	SignUpRequest,
 	SignUpResponse,
+	UpdateTaskRequest,
 } from './types'
 
 const signUp = (payload: SignUpRequest) =>
@@ -16,16 +19,34 @@ const signIn = (payload: SignInRequest) =>
 
 const fetchProjects = () => privateRequest.get<FetchProjectsResponse>('/todo/projects')
 
+const fetchProject = (id: string) =>
+	privateRequest.get<FetchProjectResponse>(`/todo/projects/${id}`)
+
 const createProject = (payload: CreateProjectRequest) =>
 	privateRequest.post('/todo/projects', payload)
+
+const deleteProject = (id: string) => privateRequest.delete(`/todo/projects/${id}`)
+
+const createTask = (payload: CreateTaskRequest) => privateRequest.post('/todo/items', payload)
+
+const updateTask = ({ id, payload }: { id: string; payload: UpdateTaskRequest }) =>
+	privateRequest.patch(`/todo/items/${id}`, payload)
+
+const deleteTask = (id: string) => privateRequest.delete(`/todo/items/${id}`)
 
 export const api = {
 	signUp,
 	signIn,
 	fetchProjects,
+	fetchProject,
 	createProject,
+	deleteProject,
+	createTask,
+	updateTask,
+	deleteTask,
 }
 
 export enum QueryKey {
 	Projects = 'projects',
+	Project = 'project',
 }
--- src/api/types.ts
+++ src/api/types.ts
@@ -22,9 +22,38 @@ export interface SignInResponse {
 }
 
 export interface FetchProjectsResponse {
-	projects: { id: string; name: string }[]
+	projects: ProjectInfo[]
 }
 
 export interface CreateProjectRequest {
 	name: string
+}
+
+export interface FetchProjectResponse {
+	info: ProjectInfo
+	items: Task[]
+}
+
+export interface CreateTaskRequest {
+	projectId: string
+	title: string
+}
+
+export interface UpdateTaskRequest {
+	title?: string
+	isDone?: boolean
+}
+
+// ####################################################################################################
+
+export interface ProjectInfo {
+	id: string
+	name: string
+}
+
+export interface Task {
+	id: string
+	title: string
+	isDone: boolean
+	project: ProjectInfo
 }
