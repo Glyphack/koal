b55ac99b272948d57773c432b9c73f49ce61a7cb
frustak <soulburn007@gmail.com>
2022-06-05 14:02:57 +0430

    Remove button and field classes

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -3,5 +3,5 @@ import { ButtonHTMLAttributes } from 'react'
 type FieldProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button(props: FieldProps) {
-	return <button className="border" {...props} />
+	return <button {...props} />
 }
--- src/features/ui/field.tsx
+++ src/features/ui/field.tsx
@@ -3,7 +3,7 @@ import { forwardRef, InputHTMLAttributes } from 'react'
 type FieldProps = InputHTMLAttributes<HTMLInputElement>
 
 export const Field = forwardRef<HTMLInputElement, FieldProps>((props, ref) => {
-	return <input className="border" {...props} ref={ref} />
+	return <input {...props} ref={ref} />
 })
 
 Field.displayName = 'Field'
