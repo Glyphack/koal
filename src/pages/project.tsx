6ccb6b364d07d423c0898fd078383c9edb49ef56
frustak <soulburn007@gmail.com>
2022-06-08 22:46:47 +0430

    Show 404 if project doesn't exists

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -4,6 +4,8 @@ import { api, QueryKey } from '../api'
 import { ProjectDeletion } from '../features/project'
 import { TaskForm, TaskList } from '../features/task'
 import { Loader } from '../features/ui'
+import { checkNotFound } from '../utils'
+import { NotFoundPage } from './404'
 
 export function ProjectPage() {
 	const { id: projectId = '' } = useParams<{ id: string }>()
@@ -18,7 +20,9 @@ export function ProjectDetails({ projectId }: ProjectDetailsProps) {
 	const projectQuery = useQuery([QueryKey.Project, projectId], () => api.fetchProject(projectId))
 	const project = projectQuery.data?.data
 
+	if (checkNotFound(projectQuery.error)) return <NotFoundPage />
 	if (projectQuery.isLoading || !project) return <Loader />
+
 	return (
 		<main>
 			<div>
