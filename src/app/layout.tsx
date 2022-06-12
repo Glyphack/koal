183f490e74c8828675452fa388d3d21c67d6ccf6
frustak <soulburn007@gmail.com>
2022-06-13 01:16:01 +0430

    Improve layout ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
 	return (
 		<div className="min-h-screen p-4 text-gray-700 selection:bg-emerald-500 selection:text-emerald-900 font-body lg:p-20">
-			<div className="max-w-xs">{children}</div>
+			<div>{children}</div>
 		</div>
 	)
 }
