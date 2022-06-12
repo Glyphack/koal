6218cab9554c33595fc9f282e7a82a0ca2da9491
frustak <soulburn007@gmail.com>
2022-06-13 01:16:08 +0430

    Improve link ui

--- src/features/ui/link.tsx
+++ src/features/ui/link.tsx
@@ -1,13 +1,21 @@
-import { HiArrowSmRight } from 'react-icons/hi'
-import { Link as RouterLink, LinkProps } from 'react-router-dom'
+import clsx from 'clsx'
+import { HiArrowSmLeft, HiArrowSmRight } from 'react-icons/hi'
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom'
 
-export function Link({ children, ...rest }: LinkProps) {
+interface LinkProps extends RouterLinkProps {
+	withLeftIcon?: boolean
+}
+
+export function Link({ children, className, withLeftIcon, ...rest }: LinkProps) {
 	return (
 		<RouterLink
 			{...rest}
-			className="flex items-center gap-1 font-semibold transition rounded-lg outline-none focus:text-emerald-800 hover:text-emerald-800 ring-offset-2 text-emerald-900 focus:ring-1 ring-emerald-300 max-w-min whitespace-nowrap"
+			className={clsx(
+				'flex items-center gap-1 font-semibold transition rounded-lg outline-none focus:text-emerald-800 hover:text-emerald-800 ring-offset-2 text-emerald-900 focus:ring-1 ring-emerald-300 max-w-min whitespace-nowrap',
+				className
+			)}
 		>
-			{children} <HiArrowSmRight />
+			{withLeftIcon && <HiArrowSmLeft />} {children} {!withLeftIcon && <HiArrowSmRight />}
 		</RouterLink>
 	)
 }
