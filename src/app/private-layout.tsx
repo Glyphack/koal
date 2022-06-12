82226f2e2a148d42fd682be4fd68624ea17a130e
frustak <soulburn007@gmail.com>
2022-06-13 01:14:17 +0430

    Add back to projects link

--- src/app/private-layout.tsx
+++ src/app/private-layout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from 'react'
 import { HiArrowSmLeft } from 'react-icons/hi'
 import { useQueryClient } from 'react-query'
+import { useMatch } from 'react-router-dom'
 import { useAuth } from '../features/auth'
-import { Button } from '../features/ui'
+import { Button, Link } from '../features/ui'
 
 interface PrivateLayoutProps {
 	children: ReactNode
 }
 
 export function PrivateLayout({ children }: PrivateLayoutProps) {
+	const isInProjectPage = useMatch('/project/:id')
 	const signOut = useAuth((state) => state.signOut)
 	const queryClient = useQueryClient()
 	const handleSignOut = () => {
@@ -18,7 +20,14 @@ export function PrivateLayout({ children }: PrivateLayoutProps) {
 
 	return (
 		<div className="space-y-10">
-			<header className="flex justify-end">
+			<header className="flex items-center justify-between gap-4">
+				<div>
+					{isInProjectPage && (
+						<Link to="/project" withLeftIcon>
+							Projects
+						</Link>
+					)}
+				</div>
 				<Button variant="text" onClick={handleSignOut}>
 					<HiArrowSmLeft /> Sign Out
 				</Button>
