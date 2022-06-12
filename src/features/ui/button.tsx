6ee2854cb8f5ae912577ef220572fa7b4591143e
frustak <soulburn007@gmail.com>
2022-06-12 21:32:27 +0430

    Implement text button variant

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -1,15 +1,22 @@
+import clsx from 'clsx'
 import { ButtonHTMLAttributes } from 'react'
 import { Loader } from './loader'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	loading?: boolean
+	variant?: 'filled' | 'text'
 }
 
-export function Button({ children, disabled, loading, ...rest }: ButtonProps) {
+export function Button({ children, variant = 'filled', disabled, loading, ...rest }: ButtonProps) {
 	return (
 		<button
 			{...rest}
-			className="w-full px-4 py-3 font-black transition rounded-lg shadow-sm outline-none hover:bg-emerald-300 bg-emerald-400 text-emerald-900 focus:ring-1 ring-emerald-300 disabled:bg-emerald-400 shadow-emerald-100 ring-offset-2 focus:bg-emerald-300"
+			className={clsx(
+				'flex justify-center items-center transition rounded-lg hover:text-emerald-800 focus:text-emerald-800 outline-none text-emerald-900 focus:ring-1 ring-emerald-300 ring-offset-2',
+				variant === 'filled' &&
+					'hover:bg-emerald-300 w-full font-black py-3 px-4 bg-emerald-400 disabled:bg-emerald-400 shadow-emerald-100 shadow-sm focus:bg-emerald-300',
+				variant === 'text' && 'font-semibold gap-1'
+			)}
 			disabled={disabled || loading}
 		>
 			{loading ? <Loader /> : children}
