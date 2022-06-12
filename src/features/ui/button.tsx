99cd318fbee82eed7e95b86025baf2d1dbedb1c9
frustak <soulburn007@gmail.com>
2022-06-13 01:15:54 +0430

    Add icon button variant

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -4,7 +4,7 @@ import { Loader } from './loader'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	loading?: boolean
-	variant?: 'filled' | 'text'
+	variant?: 'filled' | 'text' | 'icon'
 }
 
 export function Button({ children, variant = 'filled', disabled, loading, ...rest }: ButtonProps) {
@@ -15,11 +15,13 @@ export function Button({ children, variant = 'filled', disabled, loading, ...res
 				'flex justify-center items-center transition rounded-lg hover:text-emerald-800 focus:text-emerald-800 outline-none text-emerald-900 focus:ring-1 ring-emerald-300 ring-offset-2',
 				variant === 'filled' &&
 					'hover:bg-emerald-300 w-full font-black py-3 px-4 bg-emerald-400 disabled:bg-emerald-400 shadow-emerald-100 shadow-sm focus:bg-emerald-300',
-				variant === 'text' && 'font-semibold gap-1'
+				variant === 'text' && 'font-semibold gap-1',
+				variant === 'icon' &&
+					'w-10 h-10 text-lg rounded-full hover:bg-emerald-50 focus:bg-emerald-50'
 			)}
 			disabled={disabled || loading}
 		>
-			{loading ? <Loader /> : children}
+			{loading ? <Loader size={variant === 'icon' ? 18 : 36} /> : children}
 		</button>
 	)
 }
