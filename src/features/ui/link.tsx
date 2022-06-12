9241b246ce8cbc577e671d733a5f71bef1540d60
frustak <soulburn007@gmail.com>
2022-06-12 21:16:12 +0430

    Improve link ui

--- src/features/ui/link.tsx
+++ src/features/ui/link.tsx
@@ -1,5 +1,13 @@
+import { HiArrowSmRight } from 'react-icons/hi'
 import { Link as RouterLink, LinkProps } from 'react-router-dom'
 
-export function Link(props: LinkProps) {
-	return <RouterLink {...props} />
+export function Link({ children, ...rest }: LinkProps) {
+	return (
+		<RouterLink
+			{...rest}
+			className="flex items-center gap-1 font-semibold transition rounded-lg outline-none focus:text-emerald-800 hover:text-emerald-800 ring-offset-2 text-emerald-900 focus:ring-1 ring-emerald-300 max-w-min whitespace-nowrap"
+		>
+			{children} <HiArrowSmRight />
+		</RouterLink>
+	)
 }
