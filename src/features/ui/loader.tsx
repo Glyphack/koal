a55eb853e4e1a913b10762a5c4a6b2491c757c2b
frustak <soulburn007@gmail.com>
2022-06-13 01:16:15 +0430

    Improve loader ui

--- src/features/ui/loader.tsx
+++ src/features/ui/loader.tsx
@@ -2,14 +2,15 @@ import { Jelly } from '@uiball/loaders'
 
 interface LoaderProps {
 	loading?: boolean
+	size?: number
 }
 
-export function Loader({ loading = true }: LoaderProps) {
+export function Loader({ loading = true, size = 36 }: LoaderProps) {
 	if (!loading) return null
 
 	return (
 		<div data-testid="loader" className="flex items-center justify-center h-6">
-			<Jelly size={40} color="#064e3b" />
+			<Jelly size={size} color="#064e3b" />
 		</div>
 	)
 }
