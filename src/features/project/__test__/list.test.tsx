4bf314a882a4c39bc08bdba3307ca814bbca9339
frustak <soulburn007@gmail.com>
2022-06-16 15:33:51 +0430

    Fix project list test

--- src/features/project/__test__/list.test.tsx
+++ src/features/project/__test__/list.test.tsx
@@ -1,4 +1,4 @@
-import { mockProject } from '../../../test/handlers'
+import { mockProject1 } from '../../../test/handlers'
 import { render, screen, waitFor } from '../../../test/test-utils'
 import { ProjectList } from '../list'
 
@@ -8,7 +8,7 @@ describe('ProjectList', () => {
 		const loader = screen.getByTestId('loader')
 		expect(loader).toBeInTheDocument()
 		await waitFor(() => expect(loader).not.toBeInTheDocument())
-		const project = screen.getByRole('link', { name: mockProject.name })
+		const project = screen.getByRole('link', { name: mockProject1.name })
 		expect(project).toBeInTheDocument()
 	})
 })
