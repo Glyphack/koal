e98e0b7c7534ce34e8bc86abadfed2530e3394cb
frustak <soulburn007@gmail.com>
2022-06-16 15:34:52 +0430

    Add more mocks

--- src/test/handlers.ts
+++ src/test/handlers.ts
@@ -3,8 +3,11 @@ import { FetchProjectResponse, FetchProjectsResponse } from '../api'
 
 const API_URL = import.meta.env.VITE_API_URL
 
-export const mockProject = { id: 'p1', name: 'project 1' }
-export const mockTask = { id: 't1', title: 'task 1', isDone: false, project: mockProject }
+export const mockProject1 = { id: 'p1', name: 'project 1' }
+export const mockProject2 = { id: 'p2', name: 'project 2' }
+export const mockTask = { id: 't1', title: 'task 1', isDone: false, project: mockProject1 }
+export const mockDoneTask = { id: 't2', title: 'task 2', isDone: true, project: mockProject2 }
+export const mockTasks = [mockTask, mockDoneTask]
 
 export const handlers = [
 	rest.get(`${API_URL}/mock`, (req, res, ctx) => res(ctx.status(200))),
@@ -12,15 +15,22 @@ export const handlers = [
 	rest.post(`${API_URL}/auth/login`, (req, res, ctx) => res(ctx.status(200))),
 	rest.post(`${API_URL}/todo/projects`, (req, res, ctx) => res(ctx.status(200))),
 	rest.get(`${API_URL}/todo/projects`, (req, res, ctx) =>
-		res(ctx.status(200), ctx.json<FetchProjectsResponse>({ projects: [mockProject] }))
+		res(ctx.status(200), ctx.json<FetchProjectsResponse>({ projects: [mockProject1] }))
 	),
+	rest.delete(`${API_URL}/todo/projects/:id`, (req, res, ctx) => res(ctx.status(200))),
 	rest.post(`${API_URL}/todo/items`, (req, res, ctx) => res(ctx.status(200))),
 	rest.delete(`${API_URL}/todo/items/:id`, (req, res, ctx) => res(ctx.status(200))),
 	rest.patch(`${API_URL}/todo/items/:id`, (req, res, ctx) => res(ctx.status(200))),
 	rest.get(`${API_URL}/todo/projects/:id`, (req, res, ctx) =>
 		res(
 			ctx.status(200),
-			ctx.json<FetchProjectResponse>({ info: mockProject, items: [mockTask] })
+			ctx.json<FetchProjectResponse>({ info: mockProject1, items: mockTasks })
+		)
+	),
+	rest.get(`${API_URL}/todo/lists/undone`, (req, res, ctx) =>
+		res(
+			ctx.status(200),
+			ctx.json<FetchProjectResponse>({ info: mockProject1, items: [mockTask] })
 		)
 	),
 ]
