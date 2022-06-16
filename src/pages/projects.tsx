1b0a0c30c9838f2909971d8518bd88c97e959a91
frustak <soulburn007@gmail.com>
2022-06-16 12:10:02 +0430

    Add link to inbox in projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,5 @@
 import { ProjectForm, ProjectList } from '../features/project'
+import { Link } from '../features/ui'
 
 export function ProjectsPage() {
 	return (
@@ -6,7 +7,13 @@ export function ProjectsPage() {
 			<div className="max-w-xs grow">
 				<ProjectForm />
 			</div>
-			<ProjectList />
+			<div className="flex flex-col gap-10 grow">
+				<Link className="text-3xl font-normal" to="/inbox">
+					Inbox
+				</Link>
+				<hr />
+				<ProjectList />
+			</div>
 		</main>
 	)
 }
