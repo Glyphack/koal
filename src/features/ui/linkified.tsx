eb8d997718d05838ced52e9840dca37168e0e942
frustak <soulburn007@gmail.com>
2022-06-17 18:48:22 +0430

    Implement linkified component

---
+++ src/features/ui/linkified.tsx
@@ -0,0 +1,27 @@
+import urlRegex from 'url-regex'
+
+interface LinkifiedProps {
+	children: string
+}
+
+export function Linkified({ children }: LinkifiedProps) {
+	return (
+		<>
+			{children.split(' ').map((word, index) =>
+				word.match(urlRegex()) ? (
+					<a
+						key={index}
+						className="underline transition hover:text-gray-600 underline-offset-1"
+						href={word}
+						target="_blank"
+						rel="noreferrer"
+					>
+						{word}{' '}
+					</a>
+				) : (
+					<span key={index}>{word} </span>
+				)
+			)}
+		</>
+	)
+}
