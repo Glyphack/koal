52e972a28827e83140a7163b660fb2feb237f43e
frustak <soulburn007@gmail.com>
2022-08-11 16:01:52 +0430

    Reject promise on response error

--- ui/src/api/config.ts
+++ ui/src/api/config.ts
@@ -6,6 +6,7 @@ import { useAuth } from '../features/auth'
 const API_URL = import.meta.env.VITE_API_URL
 export const publicRequest = axios.create({ baseURL: API_URL })
 export const privateRequest = axios.create({ baseURL: API_URL })
+
 privateRequest.interceptors.request.use(
 	(config) => {
 		const token = Cookies.get('token')
@@ -25,6 +26,6 @@ privateRequest.interceptors.response.use(
 			useAuth.getState().signOut()
 			queryClient.clear()
 		}
-		return error
+		return Promise.reject(error)
 	}
 )
