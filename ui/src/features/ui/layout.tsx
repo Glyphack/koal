90894f5e074968eec7674487148e541bfb2fc263
frustak <soulburn007@gmail.com>
2022-09-09 13:07:31 +0430

    Center app and add container

--- ui/src/features/ui/layout.tsx
+++ ui/src/features/ui/layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
 	return (
-		<div className="min-h-screen p-4 text-gray-700 selection:bg-emerald-500 selection:text-emerald-900 font-body lg:px-20 lg:py-16">
+		<div className="container min-h-screen p-4 mx-auto text-gray-700 selection:bg-emerald-500 selection:text-emerald-900 font-body lg:px-20 lg:py-16">
 			<div>{children}</div>
 		</div>
 	)
